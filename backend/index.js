41736dedd18f1ff5bdec5dff5a705e9312d0b498
AnushkaSingh5 <153352346+AnushkaSingh5@users.noreply.github.com>
2025-08-07 15:12:28 +0530

    Final update

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const uri = process.env.MONGO_URL;
 const app = express();
 
 app.use(cors({
-  origin: ["http://localhost:3000", "http://localhost:3001"], 
+  origin: ["http://localhost:3000", "http://localhost:3001", "https://stock-frontend-q17u.onrender.com", "https://stock-dashboard-6hm4.onrender.com"], 
   credentials: true,
 }));
 
